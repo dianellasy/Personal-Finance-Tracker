76ee4647b975729f79132b0631c4db8229b6b929
Dianella Sy <dianellabsy@csu.fullerton.edu>
2026-05-14 11:02:58 -0700

    Handled the MongoDB Connection Using Mongoose

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+// Handle the MongoDB connection using Mongoose
+
+const mongoose = require("mongoose");
+
+module.exports = async function connectMongoDB() {
+    try {
+        const database_name = "cpsc431_sydianella"
+
+        // Connect to local MongoDB instance
+        await mongoose.connect('mongodb://127.0.0.1:27017/${database_name}');
+
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("Database connection error:", error);
+    }    
+};
